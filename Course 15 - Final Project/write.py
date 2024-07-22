89b88d92062c3d20bbe7e7f7850a362c1ea5639b
Yixiao Zeng <yixiaozeng0208@outlook.com>
2024-07-21 20:12:07 -0400

    update comment

--- Course 15 - Final Project/write.py
+++ Course 15 - Final Project/write.py
@@ -2,12 +2,12 @@ import csv
 
 def write_csv(file_path, data):
     """
-    Given function
     Write data to a CSV file.
     
     :param file_path: Path to the CSV file
     :param data: List of dictionaries containing the data to write
     """
+    # DON'T MODIFY THIS FUNCTION!
     if data:
         keys = data[0].keys()
         with open(file_path, mode='w', newline='') as file:
